55c751bf0a09ea75b8997c9d3dc1ce6071adafad
duynguyen <duynguyenst15@gmail.com>
2025-08-12 12:09:34 +0700

    Initial commit

---
+++ src/main/java/com/example/housewareecommerce/HousewareEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.housewareecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewareEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousewareEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/housewareecommerce/HousewareEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housewareecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousewareEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
