0f22d960982e50c770cf42aaa087817e2b2e2354
YumeChihiro <truongbaock@gmail.com>
2025-08-14 13:58:35 +0700

    test

--- src/main/java/com/example/housewareecommerce/Repository/StatusRepository.java
+++ src/main/java/com/example/housewareecommerce/Repository/StatusRepository.java
@@ -1,14 +1,7 @@
 package com.example.housewareecommerce.Repository;
 
 import com.example.housewareecommerce.Entity.StatusEntity;
-import com.example.housewareecommerce.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
 public interface StatusRepository extends JpaRepository<StatusEntity, Long> {
-
-    Optional<StatusEntity> findById(Long id);
 }
