663d1177460934c889a30d170b141a887dcf5490
duynguyen <duynguyenst15@gmail.com>
2025-08-16 20:46:10 +0700

    Initial commit

--- src/main/java/com/example/housewareecommerce/Repository/Custom/EvaluateRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.housewareecommerce.Repository.Custom;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EvaluateRepositoryCustom {
-    Integer averageRating(Long productId);
-}
