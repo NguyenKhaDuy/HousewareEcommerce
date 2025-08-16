361ab0a99f829970a7bf707f6253f35ec0455269
duynguyen <duynguyenst15@gmail.com>
2025-08-16 19:02:16 +0700

    Initial commit

---
+++ src/main/java/com/example/housewareecommerce/Model/Request/EvaluateRequest.java
@@ -0,0 +1,51 @@
+package com.example.housewareecommerce.Model.Request;
+
+import java.time.LocalDate;
+
+public class EvaluteRequest {
+    private Long id;
+    private Integer star;
+    private String note;
+    private Long userId;
+    private Long productId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getStar() {
+        return star;
+    }
+
+    public void setStar(Integer star) {
+        this.star = star;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
